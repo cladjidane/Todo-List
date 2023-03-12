9f40f81d4fdb4b370fe219d7720c5a048a721ed9
Canu Fabien <fabien.canu@gmail.com>
2023-03-12 12:29:09 +0100

    UP

--- HCJ/app.js
+++ HCJ/app.js
@@ -1,26 +1,10 @@
 /**
- * Load
- */
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://cipa3:8890/")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (tasks) {
-      tasks.map((task) => {
-        displayList(task);
-      });
-    });
-});
-
-/**
  * Manage Task
  */
 const form = document.querySelector("form");
 const list = document.querySelector("ul");
 const input = document.querySelector("form input");
 const select = document.querySelector("form select");
-let allItems = [];
 
 form.addEventListener("submit", submitTask);
 
@@ -29,7 +13,7 @@ function submitTask(event) {
   let task = !input.value ? select.value : input.value.trim();
 
   if (task !== "") {
-    addTaskInBdd(task);
+    displayList({ task: task, id: Date.now(), status: "pending" });
 
     input.value = "";
     select.value = "";
@@ -41,7 +25,6 @@ function submitTask(event) {
 function displayList(task) {
   const item = document.createElement("li");
   item.setAttribute("data-key", task.id);
-  if (task.status === "finish") item.setAttribute("class", "ok");
 
   const input = document.createElement("input");
   input.setAttribute("type", "checkbox");
@@ -65,66 +48,15 @@ function displayList(task) {
   item.appendChild(btn);
 
   list.appendChild(item);
-  allItems.push(item);
 }
 
 function updateItem(e) {
   const el = e.target.parentNode;
-  const id = el.getAttribute("data-key");
-  const status = el.getAttribute("class") !== "ok" ? "finish" : "pending";
-
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-
-  fetch(
-    "https://cipa3:8890/update-task.php?id=" + id + "&status=" + status,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then(() => el.classList.toggle("ok"))
-    .catch((error) => console.log("error", error));
+  el.classList.toggle("ok");
 }
 
 function deleteItem(e) {
   const el = e.target.parentNode;
-  const id = el.getAttribute("data-key");
-
-  if (el.getAttribute("class") !== "ok") {
-    alert("INTERDIT");
-    return;
-  }
-
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-
-  fetch("https://cipa3:8890/delete-task.php?id=" + id, requestOptions)
-    .then((response) => response.json())
-    .then((result) => notice(result.message))
-    .catch((error) => console.log("error", error));
-
-  el.remove();
-}
-
-function addTaskInBdd(task) {
-  var formdata = new FormData();
-  formdata.append("task", task);
-
-  var requestOptions = {
-    method: "POST",
-    body: formdata,
-    redirect: "follow",
-  };
-
-  fetch("https://cipa3:8890/add-task.php", requestOptions)
-    .then((response) => response.json())
-    .then((tasks) => tasks.map((task) => displayList(task)))
-    .catch((error) => console.log("error", error));
-}
-
-function notice(message) {
-  alert(message);
+  if (el.getAttribute("class") !== "ok") alert("INTERDIT");
+  else el.remove();
 }
