a0454faf7aafcccbbc53bf6ffc30554dd75272bc
Canu Fabien <fabien.canu@gmail.com>
2023-03-12 12:07:26 +0100

    Update app.js

--- HCJ/app.js
+++ HCJ/app.js
@@ -1,38 +1,20 @@
 /**
- * Load
- */
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://cipa3:8890/")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (tasks) {
-      tasks.map((task) => {
-        displayList(task);
-      });
-    });
-});
-
-/**
  * Manage Task
  */
 const form = document.querySelector("form");
 const list = document.querySelector("ul");
 const input = document.querySelector("form input");
 const select = document.querySelector("form select");
-let allItems = [];
 
 form.addEventListener("submit", submitTask);
 
 function submitTask(event) {
   event.preventDefault();
-  let task = !input.value ? select.value : input.value.trim();
+  let task = input.value.trim();
 
   if (task !== "") {
     addTaskInBdd(task);
-
     input.value = "";
-    select.value = "";
   } else {
     alert("veuillez saisir une tâche !");
   }
@@ -65,49 +47,9 @@ function displayList(task) {
   item.appendChild(btn);
 
   list.appendChild(item);
-  allItems.push(item);
 }
 
-function updateItem(e) {
-  const el = e.target.parentNode;
-  const id = el.getAttribute("data-key");
-  const status = el.getAttribute("class") !== "ok" ? "finish" : "pending";
 
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-
-  fetch(
-    "https://cipa3:8890/update-task.php?id=" + id + "&status=" + status,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then(() => el.classList.toggle("ok"))
-    .catch((error) => console.log("error", error));
-}
-
-function deleteItem(e) {
-  const el = e.target.parentNode;
-  const id = el.getAttribute("data-key");
-
-  if (el.getAttribute("class") !== "ok") {
-    alert("INTERDIT");
-    return;
-  }
-
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-
-  fetch("https://cipa3:8890/delete-task.php?id=" + id, requestOptions)
-    .then((response) => response.json())
-    .then((result) => notice(result.message))
-    .catch((error) => console.log("error", error));
-
-  el.remove();
-}
 
 function addTaskInBdd(task) {
   var formdata = new FormData();
@@ -123,8 +65,4 @@ function addTaskInBdd(task) {
     .then((response) => response.json())
     .then((tasks) => tasks.map((task) => displayList(task)))
     .catch((error) => console.log("error", error));
-}
-
-function notice(message) {
-  alert(message);
 }
