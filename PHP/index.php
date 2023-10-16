7d1485a829035df2e2e8f4bda344fb29f463916c
Canu Fabien <fabien.canu@gmail.com>
2023-10-16 21:46:00 +0200

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,19 +1,43 @@
-<?php
-$tasks = array(
-  array('task' => "Passer le balai", 'id' => 1, 'status' => "wip"),
-  array('task' => "Saluer le boss", 'id' => 2, 'status' => "finish"),
-  array('task' => "Couper l'ordi", 'id' => 1, 'status' => "wip")
-)
+<?php 
+
+include('db.class.php');
+
+$db = new Db();
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    switch ($_REQUEST['mode']) {
+        case 'add':
+            $task = $_REQUEST['field-task'] ? $_REQUEST['field-task'] : $_REQUEST['select-task'];
+            $db->addTask($task);
+            break;
+        case 'update':
+            $status = $_REQUEST['status'] == "on" ? "finish" : "pending";
+            $db->updateTask($_REQUEST['id'], $status);
+            break;
+    }
+}
+elseif ($_SERVER['REQUEST_METHOD'] == "GET") {
+    switch ($_REQUEST['mode']) {
+        case 'delete':
+            $db->deleteTask($_REQUEST['id']);
+            break;
+    }
+}
+
+$tasks = $db->getTasks();
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Todo</title>
     <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
+</head>
+
+<body>
     <h1>Todo List</h1>
     <h2>Gestionnaire de tâches</h2>
 
@@ -22,7 +46,7 @@ $tasks = array(
       <div class="fields">
         <input name="field-task" type="text" id="item" placeholder="Intitulé de la tâche" />
         ou
-        <select name="select-task" >
+        <select name="select-task">
           <option value="">Choisir une tâche</option>
           <option value="Passer le balai">Passer le balai</option>
           <option value="Saluer le boss">Saluer le boss</option>
@@ -35,28 +59,23 @@ $tasks = array(
     </form>
 
     <ul class="list-todo">
-
       <?php if($tasks) : ?>
         <?php foreach($tasks as $key => $task) : ?>
+          <li data-key="<?php echo $task->id; ?>" class="<?php echo $task->status == "finish" ? "ok": ""; ?>">
 
-          <li class="<?php echo $task['status'] == "finish" ? "ok": ""; ?>">
-            <input
-              <?php echo $task['status'] == "finish" ? "checked": ""; ?>
-              type="checkbox"
-              name="status"
-            >
-            <span>
-              <?php echo $task['task']; ?>
-            </span>
-            <a
-              href="?mode=delete&id=<?php echo $task['id']; ?>"
-              class="btdelete">
-            </a>
+            <form class="form-checked" method="post">
+              <input <?php echo $task->status == "finish" ? "checked": ""; ?> type="checkbox" name="status">
+              <input type="hidden" name="mode" value="update" />
+              <input type="hidden" name="id" value="<?php echo $task->id; ?>" />
+              <button>Valider</button>
+            </form>
+
+            <span><?php echo $task->task; ?></span>
+            <a href="?mode=delete&id=<?php echo $task->id; ?>" class="btdelete"></a>
           </li>
-
         <?php endforeach; ?>
       <?php endif; ?>
-
     </ul>
-  </body>
+</body>
+
 </html>
